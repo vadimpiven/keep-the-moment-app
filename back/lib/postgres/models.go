fbec8d13fd0fd00cf083e29c0f75d9c84faf3c28
Vadim Piven <vadim@piven.tech>
2020-11-29 21:05:51 +0300

    fix

--- back/lib/postgres/models.go
+++ back/lib/postgres/models.go
@@ -3,17 +3,18 @@ package postgres
 import "time"
 
 // pg tags documented here: https://pg.uptrace.dev/models/
+
 type (
 	User struct {
-		tableName  struct{}  `pg:"users,discard_unknown_columns"`
-		Email      string    `pg:"email,pk,type:text" json:"-" form:"-"`
-		ID         string    `gp:"id,nopk,notnull,unique,type:text" json:"id" form:"id"`
-		Username   string    `gp:"username,notnull,use_zero,type:text" json:"username" form:"username"`
-		Bio        string    `gp:"bio,notnull,use_zero,type:text" json:"bio" form:"bio"`
-		Hashtags   []string  `gp:"hashtags,notnull,array,type:jsonb" json:"hashtags" form:"hashtags"`
-		Image      string    `gp:"image,type:text" json:"image" form:"image"`
-		Birth      time.Time `gp:"birth,notnull,type:date" json:"birth" form:"birth"`
-		Registered time.Time `gp:"registered,notnull,type:date" json:"-" form:"-"`
-		DeletedAt  time.Time `pg:"deleted_at,soft_delete,type:timestamptz" json:"-" form:"-"`
+		tableName  struct{}  `pg:"users"`
+		Email      string    `pg:"email,pk" json:"-" form:"-"`
+		ID         string    `gp:"id,nopk,notnull,unique" json:"id" form:"id"`
+		Username   string    `gp:"username" json:"username" form:"username"`
+		Bio        string    `gp:"bio" json:"bio" form:"bio"`
+		Hashtags   []string  `gp:"hashtags,array" json:"hashtags" form:"hashtags"`
+		Image      string    `gp:"image" json:"image" form:"image"`
+		Birth      time.Time `gp:"birth,type:date" json:"birth" form:"birth"`
+		Registered time.Time `gp:"registered,notnull" json:"-" form:"-"`
+		DeletedAt  time.Time `pg:"deleted_at,soft_delete" json:"-" form:"-"`
 	}
 )
